0678688397ca143d66072829814ad3314a2705e8
Bjorn Persson <searon@mystic.se>
2012-10-18 12:27:44 +0200

    Design added
    Snyggare design även om den är väldigt basic och färglös.
    
    Saknar fortfarande sortering av vinnare.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-		
+
 <?php 
     include('Bowling.php');
     session_start();
@@ -9,58 +9,112 @@
         $_SESSION['plrArray'] = $playerList;
     }
     
-    function sortScores() {
-    }
-    
  ?>
  
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html;
-        charset=iso-8859-1" />
-		
-		<title>Spela lite Bowling!</title>
-    </head>
-    <body>
-        <div align="center">Nuvarande spelare:
-            <table border="1">
+<html lang="sv">
+  <head>
+    <meta charset="utf-8">
+    <title>Bowling, bowling, bowling, och disco!</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Style (aka, calling Bootstrap) -->
+    <link href="bootstrap.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Top Navigation -->
+   <div class="topbar">
+    <div class="fill">
+        <div class="container">
+            <h2>Bowling för alla!</h2>
+
+        </div>
+    </div>
+</div>
+
+    <div class="container">
+
+
+      
+      <!-- Example row of columns -->
+        <div class="row">
+            <div class="span6">
+            <h3>Välkommen!</h3>
+                <p>Som ett led i projektet <i>Bowling för alla!</i> har HL Design och Media&copy; beslutat att hålla en disco-bowling gala
+                den 22:a december 2012. Alla inkomster kommer att gå till projektet <i>Bowling för alla!</i> vars mål är att var man(och kvinna)
+                ska alla ha en chans att få fumla lite med ett bowlingklot.</p>
+                <p>För er som vill öva upp er lite innan galan finns det här en möjlighet att simulera bowling, fyll i namn på spelarna
+                (ett i taget) och kör sedan igång!</p>
+                
+                <p>
+                <form action="<?php $_PHP_SELF ?>" method="post">
+               
+                <b>Namn:</b> <input type="text" name="name" />
+                <input type="submit" name="submit" value="L&auml;gg till" />
+                
+                
+                </form>
+                </p>
+                
+            </div>
+        
+            <!-- The second column -->
+            <div class="span5" align="center">
+                <h2>&nbsp;</h2>
+                <p><h4>Lista av spelare:</h4></p>
+                <table border="1">
                 <tr>
-                    <th>Namn</th>
+                    <th style="width:100px">Namn</th>
                 </tr>
 
             <?php 
                 if($_POST['submit']) {
-                    $_SESSION['nrOfPlayers'] = new Player($_POST[name]);
-                    array_push($_SESSION['plrArray'],$_SESSION['nrOfPlayers']);
-                    $_SESSION['nrOfPlayers']++;
+                    if($_POST[name] === '' || $_POST[name] === null) {
+                    echo 'Name can not be empty or null';
+                    }
+                    else {
+                        if(array_key_exists($_POST[name],$_SESSION['plrArray'])) {
+                            echo 'A player with that name is already registered';                    
+                        }
+                        else {                    
+                            $_POST[name] = new Player($_POST[name]);
+                            array_push($_SESSION['plrArray'],$_POST[name]);
+                        }
+                    }
                 }
-	
+                
                 if($_POST['clear']) {
                     unset($_SESSION['plrArray']);
                     $playerList=array();
                     $_SESSION['plrArray'] = $playerList;
                 }
-	
+                
                 foreach($_SESSION['plrArray'] as $player) {
                 $player->addToTable();
                 }
             ?>
-        </table>
-    </div>
- 
-    <form action="<?php $_PHP_SELF ?>" method="post">
-        <div> L&auml;gg till spelare:<br />
-            Spelarens namn: <input type="text" name="name" /><br />
-            <input type="submit" name="submit" value="L&auml;gg till" />
-            <input type="submit" name="clear" value="Rensa listan" />
-	    </div>
-	    <br /><br />
-    </form>
-    
-	<form action="<?php $_PHP_SELF ?>" method="post">
-        <input type="submit" name="startGame" value="Starta spelet" />
-    </form>
-
+            </table>
+            <div>
+                    <form action="<?php $_PHP_SELF ?>" method="post">
+        <input type="submit" name="clear" value="Rensa listan" />
+        </form>
+            </div>
+           </div>
+        </div>
+      
+    <div class="row">
+        <div class="span12">
+            <!-- Resultatet visas här -->
+            <hr />
+            <p>När du är färdig är det bara att starta spelet genom att trycka på denna knapp!</p>
+            <form action="<?php $_PHP_SELF ?>" method="post">
+                <input type="submit" name="startGame" value="Starta spelet" />
+            </form>
+            
+            
 <?php
 	if($_POST['startGame']) {
 	    foreach($_SESSION['plrArray'] as $player) {
@@ -70,10 +124,10 @@
         // after game is played and scores are tallied, sort the winner to the top
         
 ?>
-	
+	<hr />
 	<table border="1">
 		<tr>
-            <th>Spelare</th>
+            <th style="width:80px">Spelare</th>
             <th colspan="2" style="width:60px">1</th>
             <th colspan="2" style="width:60px">2</th>
             <th colspan="2" style="width:60px">3</th>
@@ -96,5 +150,18 @@
     <?PHP
 	}
 	?>
-</body>
+            
+        </div>
+      
+      </div>
+<!-- Footer -->
+      <footer>
+            <hr />
+        <p>&copy; Björn Persson, 2012 <small>inte egentligen</small><br />
+        <small>Denna sida är gjort som ett arbetsprov av Björn Persson för företaget HL Design och Media</small></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+  </body>
 </html>
